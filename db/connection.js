a374ffb06f9fa5241348fae5e788c2623869d5c4
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-03 13:19:55 -0700

    fixed connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,9 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root';
-    password: 'programmingsocks';
-    database: 'employee_db';
+    user: 'root',
+    password: 'programmingsocks',
+    database: 'employee_db'
 });
 
 connection.connect();
