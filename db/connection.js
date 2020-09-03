5dcb7eeae7e6a91cd4a8da60ff5a2be1090298fe
Alex Madrigal <jam.madrigal@gmail.com>
2020-09-03 13:08:01 -0700

    more starter code

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root';
+    password: 'programmingsocks';
+    database: 'employee_db';
+});
+
+connection.connect();
+
+module.exports = connection;
